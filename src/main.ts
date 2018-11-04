64eabe4e79a4c5b3d6da7aadc6035338a897ae52
Eduard Nikolenko <eduardnikolenko@icloud.com>
2018-11-04 17:16:51 +0300

    Remove unused from dist

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { VueConstructor } from 'vue';
-import packageJson from '../package.json';
+import { version } from '../package.json';
 import { pluralize } from './filters/pluralize';
 
 export default {
   install(Vue: VueConstructor): void {
     Vue.filter('pluralize', pluralize);
   },
-  version: packageJson.version
+  version
 };
