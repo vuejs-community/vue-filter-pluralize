9fd33d0efbead7c1d305572ff180b36942c6d5a3
Eduard Nikolenko <eduardnikolenko@icloud.com>
2018-11-04 17:52:47 +0300

    Use named exports for non-global using

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,9 @@ import rollupPluginTypeScript from 'rollup-plugin-typescript';
 export default {
   experimentalCodeSplitting: true,
   input: './src/main.ts',
+  output: {
+    exports: 'named'
+  },
   plugins: [
     rollupPluginJson(),
     rollupPluginTypeScript()
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { VueConstructor } from 'vue';
 import { version } from '../package.json';
 import { pluralize } from './filters/pluralize';
 
+export { pluralize } from './filters/pluralize';
+
 export default {
   install(Vue: VueConstructor): void {
     Vue.filter('pluralize', pluralize);
