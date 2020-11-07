e62c7c01eb11015d936d5d109a53df956ed9b5c0
Eduard Nikolenko <mail@eduardnikolenko.com>
2020-11-08 00:04:42 +0300

    feat(build): Add UMD build

--- rollup.config.js
+++ rollup.config.js
@@ -15,6 +15,13 @@ export default {
       file: 'dist/vue-filter-pluralize.cjs.js',
       format: 'commonjs',
       sourcemap: true
+    },
+    {
+      exports: 'named',
+      file: 'dist/vue-filter-pluralize.umd.js',
+      format: 'umd',
+      name: 'VueFilterDateFormat',
+      sourcemap: true
     }
   ],
   plugins: [
